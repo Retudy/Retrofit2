a3f591edf8d48bb84fc3ae464dbdb4c562dd3ced
youneeo <steamdacut@gmail.com>
2023-11-22 23:37:36 +0900

    [fix] 줄정리

--- app/src/main/java/com/android/retrofit/firstAdapter.kt
+++ app/src/main/java/com/android/retrofit/firstAdapter.kt
@@ -8,22 +8,26 @@ import coil.load
 import com.android.retrofit.data.firstModel
 import com.android.retrofit.databinding.FragmentFirstItemBinding
 
-class firstAdapter(val items : MutableList<firstModel>):RecyclerView.Adapter<firstAdapter.ViewHolder>() {
+class firstAdapter(val items: MutableList<firstModel>) :
+    RecyclerView.Adapter<firstAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): firstAdapter.ViewHolder {
         //4번
-        val binding = FragmentFirstItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            FragmentFirstItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         // 뷰홀더 안에 view를넣어서 리턴해준다
         return ViewHolder(binding)
     }
+
     interface ItemClick {
-        fun onClick(view : View, position: Int)
+        fun onClick(view: View, position: Int)
     }
-    var itemClick : ItemClick? = null
+
+    var itemClick: ItemClick? = null
 
     override fun onBindViewHolder(holder: firstAdapter.ViewHolder, position: Int) {
-        if(itemClick != null){
-            holder.itemView.setOnClickListener { v->
-                itemClick?.onClick(v,position)
+        if (itemClick != null) {
+            holder.itemView.setOnClickListener { v ->
+                itemClick?.onClick(v, position)
             }
         }
         //3번. 리싸이클러뷰를 바인딩해주고
@@ -35,15 +39,12 @@ class firstAdapter(val items : MutableList<firstModel>):RecyclerView.Adapter<fir
         return items.size
     }
 
-    inner class ViewHolder(private val binding:FragmentFirstItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: FragmentFirstItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         //2번
         fun bindItems(item: firstModel) {
             binding.rvnum.text = item.num
             binding.rvPicture.load(item.picture)
-
-
-
-
 
 
         }
